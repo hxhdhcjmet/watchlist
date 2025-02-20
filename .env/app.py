57dc446e758124d859356efb697350277a906b81
Hxhdhcjmet <2871148635@qq.com>
2025-02-21 00:29:26 +0800

    Add adtabase support with Flask-SQLAchemy

--- .env/app.py
+++ .env/app.py
@@ -1,10 +1,25 @@
+# -*- coding: utf-8 -*-
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy#导入扩展类
+import os
+import sys
+import click
 app=Flask(__name__)
-# @app.route('/')
-# def hello():
-#     return '<h1>Hello Welcome!</h1><img src="https://ts1.cn.mm.bing.net/th/id/R-C.a993c3e3e1190b7ffe64d19d740b4ebc?rik=J8%2fVBdAfU7aWmw&riu=http%3a%2f%2fpic.baike.soso.com%2fp%2f20131206%2f20131206131902-927067888.jpg&ehk=Va3qMSkZSF3pgSsxtrzumpkXsgZdKaf447GQ6kqhvxo%3d&risl=&pid=ImgRaw&r=0">'
-name = 'Hxhdhcjmet'
-movies = [
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(app.root_path,'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+db=SQLAlchemy(app)#初始化扩展，传入程序实例app
+@app.route('/')
+def index():
+    user = User.query.first()  # 读取用户记录
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template('index.html', user=user, movies=movies)
+@app.cli.command()
+def forge():
+    """
+    Genarate fake data
+    """
+    name='hxhdhcjmet'
+    movies = [
     {'title': 'My Neighbor Totoro', 'year': '1988'},
     {'title': 'Dead Poets Society', 'year': '1989'},
     {'title': 'A Perfect World', 'year': '1993'},
@@ -16,6 +31,24 @@ movies = [
     {'title': 'WALL-E', 'year': '2008'},
     {'title': 'The Pork of Music', 'year': '2012'},
 ]
-@app.route('/')
-def index():
-    return render_template('index.html',name=name,movies=movies)
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+    
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+#@app.route('/')
+#def index():
+    #return render_template('index.html',name=name,movies=movies)
